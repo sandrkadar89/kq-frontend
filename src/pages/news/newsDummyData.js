f1e707f6319b10e11d96c6cacf22fc54cdec92bf
sandrkadar89 <136264854+sandrkadar89@users.noreply.github.com>
2023-12-12 12:15:29 +0300

    Update newsDummyData.js

--- src/pages/news/newsDummyData.js
+++ src/pages/news/newsDummyData.js
@@ -1,12 +1,61 @@
 import moment from "moment";
 
 export const newsData = [
+   {
+    title:
+      "Участие в 1-м Петербургском профессиональном семинаре для проектировщиков",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "article",
+  },
   {
     title:
-      "21-я Международная выставка промышленных насосов, компрессоров и трубопроводной арматуры, приводов и двигателей",
+      "Выставка «ЮГАГРО» - самое долгожданное событие этой осени в Краснодаре",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "video",
+  },
+  {
+    title:
+      "Обмен опытом и знакомство с инновационными технологиями на производстве Kaiquan в Китае",
     image: "/images/news/post1.jpg",
     date: moment(),
     category: "news",
+  },
+  {
+    title:
+      "Участие в конференции Об опыте модернизации систем водоснабжения и водоотведения в ЖКХ и промышленности",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "article",
+  },
+  {
+    title:
+      "Компания КЕЙ КЬЮ – участник выставки ЭкваТэк - 2023!",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "video",
+  },
+  {
+    title:
+      "XV конференция водоканалов России в Екатеринбурге",
+    image: "/images/news/post1.jpg",
+    date: moment(),
+    category: "news",
+  },
+  {
+    title:
+      "Семинар в Новосибирске для проектировщиков и производственных компании региона",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "article",
+  },
+  {
+    title:
+      "Всероссийский Водный Конгресс – главное событие лета 2023!",
+    image: "/images/news/post2.jpg",
+    date: moment(),
+    category: "video",
   },
   {
     title:
@@ -17,51 +66,9 @@ export const newsData = [
   },
   {
     title:
-      "Более 40 миллионов юаней! Kaiquan выиграл тендер на третий проект метро Чэнду",
-    image: "/images/news/post2.jpg",
-    date: moment(),
-    category: "video",
-  },
-  {
-    title:
-      "Полный апгрейд! Введен в эксплуатацию цех по производству погружных двигателей Kaiquan!",
+      "21-я Международная выставка промышленных насосов, компрессоров и трубопроводной арматуры, приводов и двигателей",
     image: "/images/news/post1.jpg",
     date: moment(),
     category: "news",
-  },
-  {
-    title:
-      "Более 40 миллионов юаней! Kaiquan выиграл тендер на третий проект метро Чэнду",
-    image: "/images/news/post2.jpg",
-    date: moment(),
-    category: "article",
-  },
-  {
-    title:
-      "Более 40 миллионов юаней! Kaiquan выиграл тендер на третий проект метро Чэнду",
-    image: "/images/news/post2.jpg",
-    date: moment(),
-    category: "video",
-  },
-  {
-    title:
-      "Полный апгрейд! Введен в эксплуатацию цех по производству погружных двигателей Kaiquan!",
-    image: "/images/news/post1.jpg",
-    date: moment(),
-    category: "news",
-  },
-  {
-    title:
-      "Более 40 миллионов юаней! Kaiquan выиграл тендер на третий проект метро Чэнду",
-    image: "/images/news/post2.jpg",
-    date: moment(),
-    category: "article",
-  },
-  {
-    title:
-      "Более 40 миллионов юаней! Kaiquan выиграл тендер на третий проект метро Чэнду",
-    image: "/images/news/post2.jpg",
-    date: moment(),
-    category: "video",
   },
 ];
